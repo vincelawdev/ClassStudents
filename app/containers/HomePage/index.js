df480314b319e94febbd791a9dd86d96e7b0aada
Vince Law <vince@siriusdesignlab.com.au>
2018-07-23 19:14:34 +1000

    Removed i18n, intl, react-intl, react-intl and all code that uses it.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,15 +10,13 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
     return (
       <h1>
-        <FormattedMessage {...messages.header} />
+        Testing
       </h1>
     );
   }
